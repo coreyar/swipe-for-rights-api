ac7541d3235f7e63b6dc8e18ed691c7c86495728
Corey <coreyarice@gmail.com>
2018-02-27 22:37:58 -0600

    Remove db uri

--- database/client.py
+++ database/client.py
@@ -1,3 +1,4 @@
+import os
 from mongoengine import connect
 
-connect(host='mongodb://corey:V7w1jKTDjpeg98iU@ds044577.mlab.com:44577/swipe-for-rights-dev')
+connect(host=os.environ['SWIPE_FOR_RIGHTS_DB'])
